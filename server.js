9ca7d692d30750b3304a40030be22ca1d7798668
unknown <tansuta>
2018-09-17 10:16:56 +0700

    45

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql')
+    var connection = mysql.createConnection({
+    host     : 'www.db4free.net',
+    username     : 's140390',
+    password : 'abc123**',
+    database : 'db140390'
+
 app.set('view engine', 'ejs');
 
+app.get('/',function(req,res){
+    res.render('pages/home');
 
-app.get('/Home',function(req,res){
-    res.render('pages/Home');
+});
+app.get('/home',function(req,res){
+    res.render('pages/home');
 });
 
+//Display
+app.get('/students',function(req,res){
+connection.connect()
+connection.query('SELECT * from students', function (err, rows, fields) {
+  if (err) throw err
 
-app.get('/student',function(req,res){
-    res.render('pages/student');
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
 });
-
-
-app.get('/subject',function(req,res){
-    res.render('pages/subject');
-});
-
-
-
-
 
 console.log('App is running at http://localhost:8081');
 app.listen(8081);
