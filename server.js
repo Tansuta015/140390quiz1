ffc8f2593c8463bca476caab09b621884460bc13
unknown <tansuta>
2018-09-17 10:29:39 +0700

    787

--- server.js
+++ server.js
@@ -1,32 +1,45 @@
 var express = require('express');
 var app = express();
+
+
 var mysql = require('mysql')
-    var connection = mysql.createConnection({
-    host     : 'www.db4free.net',
-    username     : 's140390',
-    password : 'abc123**',
-    database : 'db140390'
-
-app.set('view engine', 'ejs');
-
-app.get('/',function(req,res){
-    res.render('pages/home');
-
-});
-app.get('/home',function(req,res){
-    res.render('pages/home');
+var connection = mysql.createConnection({
+  host     : 'www.db4free.net',
+  user     : 's140390',
+  password : 'abc123**',
+  database : 'db140390'
 });
 
-//Display
-app.get('/students',function(req,res){
-connection.connect()
-connection.query('SELECT * from students', function (err, rows, fields) {
-  if (err) throw err
 
-  console.log('The solution is: ', rows[0].solution)
-})
+app.set('view engine','ejs');
 
-connection.end()
+app.get('/', function (req, res) {
+    res.render('pages/home');
+});
+app.get('/home', function (req, res) {
+    res.render('pages/home');
+});
+app.get('/students', function (req, res) {
+    
+    connection.connect();
+    connection.query('select * from students', function (err, rows, fields) {
+        if (err) throw err
+        res.render('pages/students',{students:rows})
+        console.log('The solution is: ', rows[0].solution)
+      })
+      
+      connection.end()
+});
+app.get('/subjects', function (req, res) {
+    connection.connect();
+    connection.query('select * from subjects', function (err, rows, fields) {
+        if (err) throw err
+        
+        res.render('pages/subjects',{subjects:rows})
+        console.log('The solution is: ', rows[0].solution)
+      })
+      
+      connection.end()
 });
 
 console.log('App is running at http://localhost:8081');
